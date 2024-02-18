ad28691f327e92a51d49f9f360e259eb484da51b
asm0dey <pavel.finkelshtein@gmail.com>
2024-02-18 17:46:37 +0100

    Fixes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM alpine
-RUN apk update && apk upgrade && apk add mariadb-client postgresql-client bash curl pv
+RUN apk update && apk upgrade && apk add mariadb-client postgresql-client bash curl pgloader
 ENV WAIT_VERSION 2.7.2
 ADD https://github.com/ufoscout/docker-compose-wait/releases/download/$WAIT_VERSION/wait /wait
 RUN chmod +x /wait
--- run.sh
+++ run.sh
@@ -5,9 +5,15 @@ set -o pipefail
 for i in lib.libavtor.sql.gz lib.libtranslator.sql.gz lib.libavtorname.sql.gz lib.libbook.sql.gz lib.libfilename.sql.gz lib.libgenre.sql.gz lib.libgenrelist.sql.gz lib.libjoinedbooks.sql.gz lib.librate.sql.gz lib.librecs.sql.gz lib.libseqname.sql.gz lib.libseq.sql.gz;
 do
   echo Downloading $i and restoring it to MariaDB
-  curl -s https://flibusta.is/sql/$i | gunzip -c | mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -h database-mariadb "$MYSQL_DATABASE"
+  FILE=/backup/$i
+  if test -e $FILE
+  then zflag=(-z "$FILE")
+  else zflag=()
+  fi
+  curl "${zflag[@]}" -o $FILE https://flibusta.is/sql/$i
+  zcat $FILE | mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -h database-mariadb "$MYSQL_DATABASE"
 done
-echo Dumping MariaDB database in Postgres compatibility format
-mysqldump -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -h database-mariadb --compatible=postgres "$MYSQL_DATABASE" > /backup/backup.sql
-echo Restoring the backup to Postgres to verify correctness
-PGPASSWORD="$POSTGRES_APPLICATION_USER_PASSWORD" psql --set ON_ERROR_STOP=on -U "$POSTGRES_APPLICATION_USER" -h datapase-postgres -d "$POSTGRES_APPLICATION_DATABASE" -f /backup/backup.sql
+echo Loading MariaDB to Postgres with pgloader
+pgloader mysql://$MYSQL_USER:$MYSQL_PASSWORD@database-mariadb/main postgresql://$POSTGRES_APPLICATION_USER:$POSTGRES_APPLICATION_USER_PASSWORD@postgres/$POSTGRES_APPLICATION_DATABASE
+echo Dumping Postgres to file
+PGUSER=$POSTGRES_APPLICATION_USER PGPASSWORD=$POSTGRES_APPLICATION_USER_PASSWORD PGDATABASE=$POSTGRES_APPLICATION_DATABASE PGHOST=postgres pg_dump > /backup/backup.sql
